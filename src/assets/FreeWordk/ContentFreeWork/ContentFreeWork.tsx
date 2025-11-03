2f703bb8ee6c7d1f4543f292eec27811dcfa018d
mobin <mobinmohammadi4321@gmail.com>
2025-11-03 19:35:07 +0330

    navbars

--- src/assets/FreeWordk/ContentFreeWork/ContentFreeWork.tsx
+++ src/assets/FreeWordk/ContentFreeWork/ContentFreeWork.tsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React from "react";
 
 export default function ContentFreeWork() {
   return (
-    <div className='grid  & > *:w-full container mr-2 ml-2 & > *:h-30  grid-cols-1 x:grid-cols-3 xs:grid-cols-2 mb-20 sm:grid-cols-4 gap-2 sm:gap-5 justify-center items-center '>
-        <div className=" bg-white rounded-sm"></div>
-        <div className=" bg-white rounded-sm"></div>
-        <div className=" bg-white rounded-sm"></div>
-        <div className=" bg-white rounded-sm"></div>
-        <div className=" bg-white rounded-sm"></div>
-        <div className=" bg-white rounded-sm"></div>
-        <div className=" bg-white rounded-sm"></div>
-        <div className=" bg-white rounded-sm"></div>
+    <div className="grid  & > *:w-full container mr-2 ml-2  & > *:h-40  grid-cols-1 x:grid-cols-3 xs:grid-cols-2 mb-20 sm:grid-cols-4 gap-4  sm:gap-5 justify-center items-center ">
+      <div className=" bg-white rounded-sm h-[200px]">
+        <img
+          className="object-cover w-full h-28 "
+          src="./Img/Supliments/1.PNG"
+          alt=""
+        />
+        <div className="flex flex-col gap-1 p-2">
+          <span className="text-xs font-Morabba-Bold ">سایت مکمل فروشی </span>
+          <div className="flex gap-1 justify-center items-center">
+            <svg className="w-3 h-3">
+              <use href="#laptop"></use>
+            </svg>
+            <span className="text-x fo-bol">ساخته شده توسط Rc</span>
+          </div>
+        </div>
+      </div>
+      <div className=" bg-white rounded-sm h-[200px]">
+        <div className="bg-white">
+          <img
+            className="object-cover w-full h-28 "
+            src="./Img/Restorants/1.PNG"
+            alt=""
+          />
+          <div className="flex flex-col gap-1 p-2">
+            <span className="text-xs font-Morabba-Bold ">
+              سایت رستوران مشابه رازینه{" "}
+            </span>
+            <div className="flex gap-1 justify-center items-center">
+              <svg className="w-3 h-3">
+                <use href="#laptop"></use>
+              </svg>
+              <span className="text-x fo-bol">ساخته شده توسط Rc</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
