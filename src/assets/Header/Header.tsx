0cbf5c32e094dfdf3fc17480a6fc58b7910790a7
mobin <mobinmohammadi4321@gmail.com>
2025-11-03 20:08:44 +0330

    navbars

--- src/assets/Header/Header.tsx
+++ src/assets/Header/Header.tsx
@@ -3,7 +3,7 @@ import Typewriter from "typewriter-effect";
 
 export default function Header() {
   return (
-    <div className="bg-primary text-white font-Dana h-48 rounded-md p-5 rounded-bl-4xl">
+    <div className="bg-primary ring-2 ring-zinc-700 text-white font-Dana h-48 rounded-md p-5 rounded-bl-4xl">
       <div className="flex justify-between w-full items-center h-full">
         {/* متن سمت چپ */}
         <div className="text-xs flex flex-col gap-2">
@@ -28,15 +28,18 @@ export default function Header() {
         {/* سمت راست: شیپ‌ها و عکس */}
         <div className="relative w-80 h-80 flex items-center justify-center">
           {/* عکس داخل شیپ دوم */}
-          <div className="absolute ">
+          <div className="absolute rounded-full overflow-hidden ring-5  ring-amber-300 ">
+            <div className="">
+
+            </div>
             <img
-              src="/Img/Mobin.jpg"
+              src="/Img/user.jpg"
               alt="مبین"
               className="object-cover w-32 ring-1 ring-amber-800"
-              style={{
-                clipPath:
-                  "polygon(35% 0, 94% 11%, 97% 73%, 62% 74%, 25% 100%, 22% 54%, 2% 15%)",
-              }}
+              // style={{
+              //   clipPath:
+              //     "polygon(35% 0, 94% 11%, 97% 73%, 62% 74%, 25% 100%, 22% 54%, 2% 15%)",
+              // }}
             />
           </div>
         </div>
