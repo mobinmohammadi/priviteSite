2ce244c125308b00ecf560acbfc4bd17fbb8a565
mobin <mobinmohammadi4321@gmail.com>
2025-11-03 19:00:34 +0330

    navbars

--- src/assets/FreeWordk/FreeWork.tsx
+++ src/assets/FreeWordk/FreeWork.tsx
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ContentFreeWork from "./ContentFreeWork/ContentFreeWork";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function FreeWork() {
+  const menus = ["شخصی", "فروشگاهی", "خبری"];
+  const [activeIndex, setActiveIndex] = useState<number | null>(0);
+  const [titleFilter, setTitleFilter] = useState<string | number>("شخصی");
+  const [isShowPost, setIsShowPost] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsShowPost(true)
+
+  }, [titleFilter]);
   return (
     // <div className='text-xs'>این بخش به زودی تکمیل میشود 🙄</div>
-    <div className="flex gap-12 flex-col  container">
-      <div className="flex justify-evenly & > *:text-xs  & > *:sm:text-sm & > *:w-24 & > *:sm:w-32 text-center & > *:cursor-pointer & > *:pt-2 & > *:pb-2   & > * >:pt-4  rounded-md text-sm bg-white">
-        <span className="active-freeWork">شخصی</span>
-        <span>فروشگاهی</span>
-        <span>خبری</span>
+    <>
+      <div className="flex gap-6  flex-col  container">
+        <div className="flex flex-col gap-5">
+          <div className="flex justify-evenly & > *:text-xs  & > *:sm:text-sm & > *:w-24 & > *:sm:w-32 text-center & > *:cursor-pointer & > *:pt-2 & > *:pb-2   & > * >:pt-4  rounded-md text-sm bg-white">
+            {menus.map((item, index) => (
+              <span
+                onClick={() => {
+                  setActiveIndex(index);
+                  setTitleFilter(item);
+                  console.log(item);
+                }}
+                className={`${activeIndex == index ? "active-freeWork" : ""}`}
+              >
+                {item}
+              </span>
+            ))}
+          </div>
+          <div className="flex gap-1 text-xs mr-3">
+            <span>فیلتر شده بر اساس پروژه های</span>
+            <span className="font-bold">{titleFilter}</span>
+          </div>
+        </div>
+          <AnimatePresence mode="wait">
+          <motion.div
+            key={activeIndex}
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 30 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="w-full"
+          >
+            <ContentFreeWork/>
+          </motion.div>
+        </AnimatePresence>
       </div>
-      <div className="flex justify-center  items-center">
-        <ContentFreeWork />
-      </div>
-    </div>
+    </>
   );
 }
